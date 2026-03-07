24c6043bb6fa9b9aa8424061297cd3743adc6b04
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-07 13:50:32 +0530

    fix(layout): ensure outlet renders route pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,12 +5,14 @@ import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div className="flex h-screen bg-[#0A0A0A] pl-64">
+    <div className="flex h-screen bg-[#0A0A0A]">
       <Sidebar />
+    
       <div className="flex-1 flex flex-col">
         <Navbar />
+    
         <main className="flex-1 overflow-y-auto">
-          <Outlet/>
+          <Outlet />
         </main>
       </div>
     </div>
