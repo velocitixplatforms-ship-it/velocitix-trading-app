e654247beee3498807af3edf3d2c14a76ceb2244
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-07 11:50:21 +0530

    Refactor Layout to use React Router Outlet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="flex h-screen bg-[#0A0A0A] pl-64">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Navbar />
         <main className="flex-1 overflow-y-auto">
-          {children}
+          <Outlet/>
         </main>
       </div>
     </div>
