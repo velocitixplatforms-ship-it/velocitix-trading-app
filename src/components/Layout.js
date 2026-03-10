e424ddeb7881083edae8408221dd2191fd06d8ce
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-10 22:08:03 +0530

    fix(layout): offset main content to prevent sidebar overlap

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,15 +6,21 @@ import Navbar from './Navbar';
 const Layout = () => {
   return (
     <div className="flex h-screen bg-[#0A0A0A]">
+
+      {/* Sidebar */}
       <Sidebar />
-    
-      <div className="flex-1 flex flex-col">
+
+      {/* Main content */}
+      <div className="flex-1 flex flex-col ml-64">
+
         <Navbar />
-    
+
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
+
       </div>
+
     </div>
   );
 };
