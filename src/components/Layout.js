d04abc5682a963de41b47964fcaba3ed5e3263da
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-13 21:50:56 +0530

    sidebar 3 Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,14 +7,21 @@ const Layout = () => {
   return (
     <div className="flex h-screen bg-[#0A0A0A]">
 
+      {/* TEST BAR - DO NOT REMOVE YET */}
+      <div style={{position:'fixed',top:0,left:0,zIndex:99999,background:'red',color:'white',padding:'6px'}}>
+        LAYOUT TEST
+      </div>
+
       {/* Sidebar */}
       <Sidebar />
 
       {/* Main Content */}
       <div className="flex-1 flex flex-col ml-64">
 
+        {/* Navbar */}
         <Navbar />
 
+        {/* Page Content */}
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
