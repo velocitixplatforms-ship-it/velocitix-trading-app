f57cc5607384d86ca01275e8f11f44724c7eeab8
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-13 21:44:20 +0530

    sidebar mod Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,14 +7,18 @@ const Layout = () => {
   return (
     <div className="flex h-screen bg-[#0A0A0A]">
 
+      {/* Sidebar */}
       <Sidebar />
 
+      {/* Main Content */}
       <div className="flex-1 flex flex-col ml-64">
+
         <Navbar />
 
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
+
       </div>
 
     </div>
