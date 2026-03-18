25e4850e34d843ea0ba44e1914f7a3444d599ac0
velocitixplatforms-ship-it <velocitixplatforms@gmail.com>
2026-03-18 23:05:08 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,14 +5,17 @@ import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div className="flex h-screen bg-[#0A0A0A] pl-64">
+    <div className="flex h-screen bg-[#0A0A0A]">
+
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+
+      <div className="flex-1 flex flex-col ml-64">
         <Navbar />
-        <main className="flex-1 overflow-y-auto">
+        <main className="flex-1 overflow-auto">
           <Outlet />
         </main>
       </div>
+
     </div>
   );
 };
